05dc71ae4e6aeecb2b6af61b12e9c5dc27964b96
alba-rm <alba.r.m.722@gmail.com>
2023-09-29 09:40:26 +0200

    movement

---
+++ Assets/Scripts/IsometricController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsometricController : MonoBehaviour
+{
+    private CharacterController _controller;
+    private float _horizontal;
+    private float _vertical;
+    [SerializeField] private float playerSpeed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _horizontal = Input.GetAxisRaw("Horizontal");
+        _vertical = Input.GetAxisRaw("Vertical");
+        Movement();
+    }
+
+    void Movement()
+    {
+        Vector3 direction = new Vector3(_horizontal, 0, _vertical);
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, targetAngle, 0);
+        _controller.Move(direction * playerSpeed * Time.deltaTime);
+    }
+}
