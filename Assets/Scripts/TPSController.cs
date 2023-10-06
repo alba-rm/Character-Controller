dff38a1cc61cb3ccecf142aee1ab0580cc617cee
alba-rm <alba.r.m.722@gmail.com>
2023-10-06 13:23:03 +0200

    camera

--- Assets/Scripts/TPSController.cs
+++ Assets/Scripts/TPSController.cs
@@ -9,6 +9,7 @@ public class TPSController : MonoBehaviour
     private float _horizontal;
     private float _vertical;
    
+   
     [SerializeField] private float _playerSpeed = 5;
  
     [SerializeField] private float _jumpHeight = 1;
@@ -35,8 +36,18 @@ public class TPSController : MonoBehaviour
     {
         _horizontal = Input.GetAxisRaw("Horizontal");
         _vertical = Input.GetAxisRaw("Vertical");
-        Movement();
+        if(Input.GetButton("Fire2"))
+        {
+            AimMovement();
+        
+        }
+        else
+        {
+            Movement();
+        }
+        
         Jump();
+        
     }
 
     void Movement()
@@ -65,5 +76,19 @@ public class TPSController : MonoBehaviour
         }
         _playerGravity.y += _gravity * Time.deltaTime;
         _controller.Move(_playerGravity * Time.deltaTime);
+    }
+     void AimMovement()
+    {
+        Vector3 direction = new Vector3(_horizontal, 0, _vertical);
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _camera.eulerAngles.y;
+        float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, _camera.eulerAngles.y, ref turnSmoothVelocity, turnSmoothTime);
+        transform.rotation = Quaternion.Euler(0, smoothAngle, 0);
+
+        if(direction != Vector3.zero)
+        {
+            
+            Vector3 moveDirection = Quaternion.Euler(0,targetAngle, 0) * Vector3.forward;
+            _controller.Move(moveDirection.normalized * _playerSpeed * Time.deltaTime);
+        }
     }
 }
